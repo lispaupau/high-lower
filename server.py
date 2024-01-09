aa590b895ac777d396185eaf997a2f37803700ec
pndao <pndao@inbox.ru>
2024-01-09 20:17:19 +0200

    Initial commit

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+rnd_number = random.randint(0, 9)
+
+
+@app.route('/')
+def hello():
+    return ('<h1>Guess a number between 0 and 9</h1>'
+            '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif">')
+
+
+@app.route('/<user>')
+def guess_number(user):
+    if int(user) < rnd_number:
+        return ('<h1>Too low, try again!</h1>'
+                '<img src="https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif">')
+    if int(user) > rnd_number:
+        return ('<h1>Too high, try again!</h1>'
+                '<img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif">')
+    else:
+        return ('<h1>You found me!</h1>'
+                '<img src="https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif">')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
